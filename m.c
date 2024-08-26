64b1c160132b524d4e7a0585fe17c783a7064a97
Pansarekomal039 <161268248+Pansarekomal039@users.noreply.github.com>
2024-08-26 10:22:08 +0530

    Add files via upload

--- m.c
+++ m.c
@@ -1,13 +1,29 @@
-#include<stdio.h>
-int main()
-{
-int mat[3][3],r,c;
-printf("Enter the numbers");
-for (r=0;r<3;r++)
-{
-for(c=0;c<3;c++)
-{
-scanf("%d",&mat[r][c]);
-}
-}
-}
+#include <stdio.h> 
+ 
+int main() { 
+    int rows, cols; 
+    printf("Enter number of rows: "); 
+    scanf("%d", &rows); 
+    printf("Enter number of columns: "); 
+    scanf("%d", &cols); 
+ 
+    int matrix[rows][cols]; 
+    printf("Enter matrix elements:\n"); 
+    for (int i = 0; i < rows; i++) { 
+        for (int j = 0; j < cols; j++) { 
+            scanf("%d", &matrix[i][j]); 
+        } 
+    } 
+ 
+    printf("Matrix:\n"); 
+    for (int i = 0; i < rows; i++) { 
+        for (int j = 0; j < cols; j++) { 
+            printf("%d", matrix[i][j]); 
+            } 
+            printf("\n"); 
+        } 
+     
+        return 0; 
+    } 
+
+
