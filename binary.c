0a135a51b864ed11755cd58401c4ca743dbba3a8
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2024-09-06 14:38:19 +0530

    Add files via upload

---
+++ binary.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n,m;
+    int mat[10][10];
+    printf("Enter the row of matrix;");
+    scanf("%d",&m);
+    printf("Enter the col of matrix");
+    scanf("%d",&n);
+    printf("Enter Matrix Element");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    printf("\n Matrix");
+    printf("\n");
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",mat[i][j]);
+        }
+        printf("\n");
+    }
+    for(i=0;i<m;i++){
+        if(m[i]==1)
+        {
+        max=m[i];
+        max++;
+        }
+    }
+}
