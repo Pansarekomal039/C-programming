748b720ae46529a26272c6232e8b2b7cc2667dd1
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2024-09-08 10:41:54 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int size,i,largest,n;
+    printf("Enter size of array");
+    scanf("%d",&size);
+    int array[size];
+    printf("enter element of array");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+        largest=array[0];
+        for(i=0;i<size;i++)
+        {
+            if(largest <array[i])
+            largest=array[i];
+        }
+        printf("largest element in array is%d",largest);
+        return 0;
+    
+}
