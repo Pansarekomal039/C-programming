5100ebf326a7216205a88b5fbbb86425248c5dca
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2024-09-15 12:31:04 +0530

    Update

---
+++ h.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void hello();
+void goodbye ();
+
+int main() {
+    
+    hello();
+     goodbye();
+    return 0;
+}
+void hello(){
+    printf("hello\n");
+    printf("Anurag\n");
+}
+
+void goodbye(){
+    printf("goodbye\n");
+}
