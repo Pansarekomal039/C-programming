e9848ffba050e1a2c6be969d66e6eb17a957d014
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2024-09-17 15:34:37 +0530

    Update

---
+++ evenodd.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+void checkOddEven(int N) {
+  
+    // Find the remainder
+    int r = N % 2;
+
+    // Condition for even
+    if (r == 0)  {
+        printf("Even");
+    }
+  
+    // Condition for odd number
+    else  {
+        printf("Odd");
+    }
+}
+
+int main() {
+    int N = 101;
+    checkOddEven(N);
+    return 0;
+}
