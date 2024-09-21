41888f6b0094737ec97524f8afbd626992b3d02c
pansarekomal <pansarekomal039@gmail.com>
2024-09-21 19:21:45 +0530

    Initial commit

---
+++ qu2b.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool checkDuplicatesOrDistinct(int arr[], int n) {
+    bool seen[1000] = {false}; 
+
+    for (int i = 0; i < n; i++) {
+        if (seen[arr[i]]) { 
+            return true;
+        }
+        seen[arr[i]] = true; 
+    }
+
+    return true;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6}; 
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    if (checkDuplicatesOrDistinct(arr, n)) {
+        printf("Condition met: either a value appears twice or all elements are distinct.\n");
+    } else {
+        printf("Condition not met.\n");
+    }
+
+    return 0;
+}
