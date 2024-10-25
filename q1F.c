c2463f4970c01bd36dd386fece5d75bf8a581c7e
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2024-10-25 08:57:08 +0530

    Add files via upload

---
+++ q1F.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i,arr[10];
+    int count=0;
+    printf("Enter the number");
+    scanf("%d",&arr[10]);
+    printf("The numbers are ");
+    for(i=0;i<=arr[i];i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    for(i=0;i<=arr[i];i++)
+    {
+    if(arr[i]%2!=0)
+    {
+       count++;
+    }
+}
+    printf("the odd count = %d",count);
+    return 0;
+}
