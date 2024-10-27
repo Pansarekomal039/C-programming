e496622eb1d0e8980f7ac54fbae8ea6e5e6e07cf
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2024-10-27 20:09:11 +0530

    Update Functions

---
+++ Functions/F1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Function declarations
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
+
+int main() {
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("Sum: %d\n", add(num1, num2));
+    printf("Difference: %d\n", subtract(num1, num2));
+    printf("Product: %d\n", multiply(num1, num2));
+
+    if (num2 != 0) {
+        printf("Quotient: %.2f\n", divide(num1, num2));
+    } else {
+        printf("Division by zero is not allowed.\n");
+    }
+
+    return 0;
+}
+
+// Function definitions
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(int a, int b) {
+    return (float)a / b;
+}
