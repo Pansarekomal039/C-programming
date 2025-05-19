077269d52f75a21b90da26499e38886c94887205
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2025-05-19 08:54:22 +0530

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int climbStairs(int n) {
-    if (n <= 2) return n;
+    if (n <= 2) return n; 
     int* dp = (int*) malloc((n + 1) * sizeof(int));
     dp[1] = 1;
     dp[2] = 2;
