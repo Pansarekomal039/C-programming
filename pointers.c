85cc05a69fc7c8a1bdfa90027849ab3784f6b761
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2025-07-18 15:04:27 +0530

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -4,7 +4,7 @@
 int climbStairs(int n) {
     if (n <= 2) return n; 
     int* dp = (int*) malloc((n + 1) * sizeof(int));
-    dp[1] = 1;
+    dp[1] = 1; 
     dp[2] = 2;
     for (int i = 3; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
