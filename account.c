124e9b9837aaea803fa2bc9a5ec39750031f6567
Pansare Komal <161268248+Pansarekomal039@users.noreply.github.com>
2025-08-27 16:20:00 +0530

    Update account.c

--- account.c
+++ account.c
@@ -1,7 +1,7 @@
 #include<stdio.h> 
 int main()  
 {
-    int m,n,i,j,sum=0,rowToAdd,max;
+     int m,n,i,j,sum=0,rowToAdd,max;
     printf("Enter the number of rows");
     scanf("%d",&m);
     printf("Enter the number of cols");
